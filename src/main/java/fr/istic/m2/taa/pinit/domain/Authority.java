b91fbacbbd83e5791f4abde24bc7831b09490402
Unknown <beaulielloic@gmail.com>
2017-09-22 14:35:23 +0200

    add service user

--- src/main/java/fr/istic/m2/taa/pinit/domain/Authority.java
+++ src/main/java/fr/istic/m2/taa/pinit/domain/Authority.java
@@ -9,10 +9,43 @@ import javax.validation.constraints.Size;
 @Entity
 public class Authority {
 
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+    public static final String ANONYMOUS = "ANONYMOUS";
+
     @NotNull
     @Size(min = 0, max = 50)
     @Id
     @Column(length = 50)
     private String name;
 
+    public Authority(){
+
+    }
+
+    public Authority(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Authority{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Authority authority = (Authority) o;
+
+        return name != null ? name.equals(authority.name) : authority.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
--- src/main/java/fr/istic/m2/taa/pinit/domain/User.java
+++ src/main/java/fr/istic/m2/taa/pinit/domain/User.java
@@ -23,6 +23,35 @@ public class User {
 
     private Set<Authority> authorities = new HashSet<>();
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Authority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<Authority> authorities) {
+        this.authorities = authorities;
+    }
 }
