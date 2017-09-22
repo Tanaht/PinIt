3ae5312dceba799d2b65f18308d5c73e7c27e7a5
Unknown <beaulielloic@gmail.com>
2017-09-22 10:03:56 +0200

    init project

---
+++ src/main/java/fr/istic/m2/taa/pinit/PinItApplication.java
@@ -0,0 +1,12 @@
+package fr.istic.m2.taa.pinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinItApplication.class, args);
+	}
+}
