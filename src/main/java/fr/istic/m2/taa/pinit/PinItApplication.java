4b54e5992f2a912cbc0e72b41358f6401196d7a4
Tanaky <charp.antoine@gmail.com>
2017-09-23 18:13:51 +0200

    Integrate client with server +1
    Now client is served when request matche "/" url
    (There still security issue but it seems to work  a little more

--- src/main/java/fr/istic/m2/taa/pinit/PinItApplication.java
+++ src/main/java/fr/istic/m2/taa/pinit/PinItApplication.java
@@ -37,4 +37,24 @@ public class PinItApplication {
 				env.getProperty("server.port"),
 				env.getActiveProfiles());
 	}
+
+	/*@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+
+		return args -> {
+
+			System.out.println(ctx.getBean("welcomePageHandlerMapping").getClass().toString());
+
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+
+		};
+
+	}*/
+
 }
