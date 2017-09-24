7959f14cb329cdbcb4e62eebdc815a2f7c24da5d
Unknown <beaulielloic@gmail.com>
2017-09-24 14:48:52 +0200

    add authenticate rest service

--- src/main/java/fr/istic/m2/taa/pinit/repository/AuthorityRepository.java
+++ src/main/java/fr/istic/m2/taa/pinit/repository/AuthorityRepository.java
@@ -2,9 +2,11 @@ package fr.istic.m2.taa.pinit.repository;
 
 import fr.istic.m2.taa.pinit.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data JPA repository for the Authority entity.
  */
+@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
 }
---
+++ src/main/java/fr/istic/m2/taa/pinit/service/AuthenticateService.java
@@ -0,0 +1,9 @@
+package fr.istic.m2.taa.pinit.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticateService {
+
+}
