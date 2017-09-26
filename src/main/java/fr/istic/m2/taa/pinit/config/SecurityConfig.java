a71159263669c8db6244b0473a897629469adbab
Unknown <beaulielloic@gmail.com>
2017-09-26 11:33:51 +0200

    token generation

--- src/main/java/fr/istic/m2/taa/pinit/config/SecurityConfig.java
+++ src/main/java/fr/istic/m2/taa/pinit/config/SecurityConfig.java
@@ -1,8 +1,7 @@
 package fr.istic.m2.taa.pinit.config;
 
-import fr.istic.m2.taa.pinit.domain.Authority;
-import fr.istic.m2.taa.pinit.security.jwt.CustomBasicAuthenticationFilter;
-import fr.istic.m2.taa.pinit.security.jwt.TokenAuthenticationFilter;
+import fr.istic.m2.taa.pinit.security.jwt.JWTFilter;
+import fr.istic.m2.taa.pinit.security.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +19,14 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-        public static final String AUTHORIZATION_HEADER = "headerToken";
+    public static final String AUTHORIZATION_HEADER = "headerToken";
+
+    private TokenProvider tokenProvider;
+
+    public SecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -38,27 +44,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
         //Implementing Token based authentication in this filter
-        final TokenAuthenticationFilter tokenFilter = new TokenAuthenticationFilter();
+        final JWTFilter tokenFilter = new JWTFilter(tokenProvider);
         http.addFilterBefore(tokenFilter, BasicAuthenticationFilter.class);
 
-        //Creating token when basic authentication is successful and the same token can be used to authenticate for further requests
-        final CustomBasicAuthenticationFilter customBasicAuthFilter = new CustomBasicAuthenticationFilter(this.authenticationManager() );
-        http.addFilter(customBasicAuthFilter);
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
+        /*auth
                 .inMemoryAuthentication()
                 .withUser("user").password("user").roles(Authority.USER).
                 and()
-                .withUser("admin").password("admin").roles(Authority.ADMIN);    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+                .withUser("admin").password("admin").roles(Authority.ADMIN);
+                */
     }
+
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/fr/istic/m2/taa/pinit/service/AuthenticateService.java
+++ src/main/java/fr/istic/m2/taa/pinit/service/AuthenticateService.java
@@ -1,9 +1,49 @@
 package fr.istic.m2.taa.pinit.service;
 
 
+import fr.istic.m2.taa.pinit.domain.User;
+import fr.istic.m2.taa.pinit.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthenticateService {
 
+    private final Logger log = LoggerFactory.getLogger(AuthenticateService.class);
+
+    private UserRepository userRepository;
+
+    public AuthenticateService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getPrincipal() + "";
+        String password = authentication.getCredentials() + "";
+
+        Optional<User> potentialUser = userRepository.findOneByLogin(username.toLowerCase());
+
+        log.debug("test : {}, {}",username, password);
+
+
+        if (!potentialUser.isPresent()) {
+            //bad login
+            throw new BadCredentialsException("1000");
+        }
+
+        if (!potentialUser.get().getPassword().equals(password)){
+            //bad password
+            throw new BadCredentialsException("1000");
+        }
+
+        return new UsernamePasswordAuthenticationToken(username, password, potentialUser.get().getAuthorities());
+
+    }
 }
