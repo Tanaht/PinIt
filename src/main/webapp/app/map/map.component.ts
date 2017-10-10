47a0620068e7766e8cc746c32b930b90e4663839
Tanaky <charp.antoine@gmail.com>
2017-10-10 16:51:53 +0200

    Enable loading of markers on the map

--- src/main/webapp/app/map/map.component.ts
+++ src/main/webapp/app/map/map.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivityMarker} from '../model/activity-marker';
 
 @Component({
   selector: 'map-view',
   templateUrl: './map.component.html',
-  styles: ['mat-card-content{height: 300px}']
+  styles: ['mat-card-content{height: 400px}']
 })
 export class MapComponent implements OnInit {
   lat: number;
   long: number;
-
-  constructor() { }
+  markers: ActivityMarker[];
 
   ngOnInit() {
-    this.lat = 48.115;
-    this.long = -1.638;
+    this.lat = 46.1369301;
+    this.long = -2.4342062;
+
+    this.loadMarkers();
   }
 
+  private loadMarkers(): void {
+      // here load markers from rest, but now we used a static source;
+      this.markers = [
+          new ActivityMarker(49.335, -0.454, "Surf"),
+          new ActivityMarker(48.8544586, 2.3388662, "Balade en ville"),
+          new ActivityMarker(45.892, -3.093, "Noyade"),
+          new ActivityMarker(48.1148993, -1.6370679, "Etude"),
+      ];
+  }
 }
---
+++ src/main/webapp/app/model/activity-marker.ts
@@ -0,0 +1,11 @@
+export class ActivityMarker {
+    lat: number;
+    long: number;
+    sport: string;
+
+    constructor(lat: number, long: number, sport: string) {
+        this.lat = lat;
+        this.long = long;
+        this.sport = sport;
+    }
+}
