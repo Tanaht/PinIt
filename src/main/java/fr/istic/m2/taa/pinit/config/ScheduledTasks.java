ec8d2e1831ad85c77685fd8a96733559da9c97a8
Unknown <beaulielloic@gmail.com>
2017-11-01 14:41:52 +0100

    add scheduled config

---
+++ src/main/java/fr/istic/m2/taa/pinit/config/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package fr.istic.m2.taa.pinit.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    //@Scheduled(fixedRate = 1000)
+    @Scheduled(cron = "0 0 18 * * FRI")//tout les jeudi à 18h
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
